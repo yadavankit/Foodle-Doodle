0e56aac3d954868f2c0bc2d3420304ac667a1173
techjunkie007 <connectankityadav@gmail.com>
2015-10-07 00:57:04 +0530

    Refactors main Blade Template

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,50 +1,30 @@
 <html>
-<head>
-    <title>
 
-        @yield('title')
+    <head>
+        <title>
 
-    </title>
-</head>
-<body>
-<link rel="stylesheet" href="/FoodleApp/resources/assets/css/bootstrap.css">
-<link rel="stylesheet" href="/FoodleApp/resources/assets/css/rotating-card.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+            @yield('title')
 
-<!-- Yield mid content here -->
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container-fluid" >
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">FoodleDoodle</a>
+        </title>
+    </head>
+
+    <body>
+
+        {{-- Card Rotate CSS, Bootstrap CSS, etc--}}
+        <link rel="stylesheet" href="/FoodleApp/resources/assets/css/bootstrap.css">
+        <link rel="stylesheet" href="/FoodleApp/resources/assets/css/rotating-card.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+        {{-- Top Navigation Bar --}}
+        @include('topnavbar')
+
+        {{-- Jumbotron --}}
+        <div class="jumbotron" style= "background-color: #8C2F39; color:white;">
+            <center><h1>FoodleDoodle</h1></center>
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
+        {{-- Yield Content --}}
+        @yield('content')
 
-            </ul>
-            <form class="navbar-form navbar-right" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="I Wanna Have...">
-                </div>
-                <button type="submit" class="btn btn-default">Search</button>
-            </form>
-            <ul class="nav navbar-nav">
-                <li class="dropdown navbar-right">
-                    <a href="#">Cart <span class="badge">10</span></a>
-                </li>
-                    </ul>
-                </li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
-
-<div class="jumbotron" style= "background-color: #8C2F39; color:white;">
-    <center><h1>FoodleDoodle</h1></center>
-</div>
-@yield('content')
-
-</body>
+    </body>
 </html>
---
+++ resources/views/topnavbar.blade.php
@@ -0,0 +1,32 @@
+{{-- Top Navigation Bar --}}
+<nav class="navbar navbar-default navbar-fixed-top">
+
+    <div class="container-fluid" >
+        {{-- Navbar Header --}}
+        <div class="navbar-header">
+            {{-- Brand Logo --}}
+            <a class="navbar-brand" href="#">FoodleDoodle</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            {{-- Search Box Form --}}
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="I Wanna Have...">
+                </div>
+                <button type="submit" class="btn btn-default">Search</button>
+            </form>
+
+            {{-- Display Cart --}}
+            <ul class="nav navbar-nav">
+                <li class="dropdown navbar-right">
+                    <a href="#">Cart <span class="badge">10</span></a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</nav>
